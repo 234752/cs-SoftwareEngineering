6816a1d3eb8c3899394a1974ef8cf482cc751e49
Patryk <patryk.gielcz@gmail.com>
2022-12-10 12:30:02 +0100

    buttons and on click events

--- Minigames/Assets/Classes/TransportMinigame.cs
+++ Minigames/Assets/Classes/TransportMinigame.cs
@@ -14,11 +14,13 @@ public class TransportMinigame : MonoBehaviour, IMinigame
     public GameObject transport2;
     public GameObject transport3;
     public GameObject transport4;
+    public GameObject result;
 
     public TextMeshProUGUI transport1Text;
     public TextMeshProUGUI transport2Text;
     public TextMeshProUGUI transport3Text;
     public TextMeshProUGUI transport4Text;
+    public TextMeshProUGUI resultText;
 
     #endregion
 
@@ -35,6 +37,7 @@ public class TransportMinigame : MonoBehaviour, IMinigame
         new MeanOfTransport("Tram", 1),
         new MeanOfTransport("Train", 1),
     };
+    private List<MeanOfTransport> displayedAvailableTransport;
 
     public void Start()
     {
@@ -49,7 +52,7 @@ public class TransportMinigame : MonoBehaviour, IMinigame
     public List<MeanOfTransport> DisplayListOfAvailableTransport()
     {
         List<MeanOfTransport> allMeansOfTransport = meansOfTransport;
-        List<MeanOfTransport> availabeMeansOfTransport = new List<MeanOfTransport>();
+        
 
         for (int i = 0; i < allMeansOfTransport.Count; i++)
         {
@@ -64,13 +67,15 @@ public class TransportMinigame : MonoBehaviour, IMinigame
 
     public void StartMinigame()
     {
+        resultText = result.GetComponent<TextMeshProUGUI>();
         transport1Text = transport1.GetComponent<TextMeshProUGUI>();
         transport2Text = transport2.GetComponent<TextMeshProUGUI>();
         transport3Text = transport3.GetComponent<TextMeshProUGUI>();
         transport4Text = transport4.GetComponent<TextMeshProUGUI>();
 
-        List<MeanOfTransport> displayedAvailableTransport = DisplayListOfAvailableTransport();
+        displayedAvailableTransport = DisplayListOfAvailableTransport();
 
+        resultText.text = "";
         transport1Text.text = displayedAvailableTransport[0].Name;
         transport2Text.text = displayedAvailableTransport[1].Name;
         transport3Text.text = displayedAvailableTransport[2].Name;
@@ -78,11 +83,36 @@ public class TransportMinigame : MonoBehaviour, IMinigame
 
         //Choosing the mean of transport...
 
-        CalculateEnvironmentPoints(displayedAvailableTransport[0]);
+        //CalculateEnvironmentPoints(displayedAvailableTransport[0]);
     }
 
     public void CalculateEnvironmentPoints(MeanOfTransport chosenTransport)
     {
         EnvironmentPoints = chosenTransport.PointsValue;
+        resultText.text = "Your score: " + EnvironmentPoints.ToString();
+    }
+
+    public void T1ButtonOnClick()
+    {
+        CalculateEnvironmentPoints(displayedAvailableTransport[0]);
+        transport1Text.color = Color.blue;
+    }
+
+    public void T2ButtonOnClick()
+    {
+        CalculateEnvironmentPoints(displayedAvailableTransport[1]);
+        transport2Text.color = Color.blue;
+    }
+
+    public void T3ButtonOnClick()
+    {
+        CalculateEnvironmentPoints(displayedAvailableTransport[2]);
+        transport3Text.color = Color.blue;
+    }
+
+    public void T4ButtonOnClick()
+    {
+        CalculateEnvironmentPoints(displayedAvailableTransport[3]);
+        transport4Text.color = Color.blue;
     }
 }
